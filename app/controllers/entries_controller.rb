566abbf86545c2f305c812e47d315633f58d3383
ruby-v <olga.lazarev@interia.eu>
2017-09-16 16:57:38 +0200

    first commit

---
+++ app/controllers/entries_controller.rb
@@ -0,0 +1,18 @@
+class EntriesController < ApplicationController
+  def index
+    @entries = Entry.all
+    @websites = [
+      ["http://railsgirls.com", "Rails Girls"],
+      ["https://en.wikibooks.org/wiki/Ruby_Programming", "Wikibooks"],
+      ["http://guides.rubyonrails.org", "Ruby on Rails Guides"],
+    ]
+    render 'entries/index'
+
+
+  end
+    def show
+      binding.pry
+    @entry = Entry.find(params["id"])
+  render 'entries/show'
+  end
+end
