7f6f52162004b92afe3e683831ec32e7c851d1fb
ruby-v <olga.lazarev@interia.eu>
2017-09-16 17:23:16 +0200

    apdates routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,5 @@
 Rails.application.routes.draw do
-  get 'welcome/index', to: 'welcome#index'
-  get '/', to: 'welcome#index'
-  get '/dziennik', to: 'welcome#index'
-  resources:"entries"
+  get '/', to: 'entries#index'
+  get '/entries', to: 'entries#index'
+  get '/entries/:id', to: 'entries#show', as:'entry'
 end
